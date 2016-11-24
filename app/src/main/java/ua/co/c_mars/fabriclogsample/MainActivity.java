124380fdaf3221ab8db3cc2bf68ef02d6b61c7b3
Constantine Mars <cmars@cmars-book.local>
2016-11-24 23:28:28 +0200

    Initial commit

---
+++ app/src/main/java/ua/co/c_mars/fabriclogsample/MainActivity.java
@@ -0,0 +1,75 @@
+package ua.co.c_mars.fabriclogsample;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
+
+public class MainActivity extends AppCompatActivity {
+
+    int count = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ++count;
+
+                switch (count) {
+                    case 1:
+                        Crashlytics.log(String.format("count = %d",count));
+                        break;
+                    case 2:
+                        Crashlytics.setInt("countValue", count);
+                        break;
+                }
+
+                Snackbar.make(view, String.format("count=%d",count), Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void forceCrash(View view) {
+        Crashlytics.log(String.format("right before exception count = %d",count));
+        Crashlytics.setInt("right before exception countValue", count);
+        throw new RuntimeException("This is a crash");
+    }
+
+}
